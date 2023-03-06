dfbada5f36c3cce992f8a8ec2e6b9d848fa7b7f9
Mujahid Raj <113965511+mujahidraj@users.noreply.github.com>
2023-03-07 00:01:40 +0600

    Add files via upload

---
+++ Carspeed.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace ConsoleApplication5
+{
+    class CarSpeed
+    {
+        public float acceleration;
+        public string engine_number;
+        public bool start;
+        public int time; CarSpeed()
+        {
+        }
+        void StartCar()
+        {
+            if (start == true)
+            {
+                Console.WriteLine("Car Started Succesfully");
+                this.velocity(acceleration, time);
+            }
+            else
+            {
+                Console.WriteLine("No velocity Available");
+            }
+        }
+        void StopCar()
+        {
+            if (start == false) { Console.WriteLine("Car Stoped Succesfully"); }
+        }
+        void setaccelaration(float acceleration)
+        {
+            this.acceleration = acceleration;
+        }
+        float getaccelaration()
+        {
+            return acceleration;
+        }
+        void setengine_number(string engine_number)
+        { this.engine_number = engine_number; }
+        string getengine_number()
+        {
+            return engine_number;
+        }
+        void setstart(bool start)
+        { this.start = start; }
+        int getstart()
+        {
+            return time;
+        }
+        void settime(int time)
+        { this.time = time; }
+        int gettime()
+        {
+            return time;
+        }
+        void velocity(float acceleration, int time)
+        {
+            Console.WriteLine("The velocity is : "+acceleration * time); 
+        }
+        void showdetails()
+        {
+            this.StartCar();
+            Console.WriteLine("Acceleration Is : "+acceleration);
+            Console.WriteLine("Engine Number of the car is : "+engine_number);
+            Console.WriteLine("Time Since Started : "+time);
+            
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 3; i++)
+            {
+                Console.WriteLine("Enter the Accelaration :");
+                float acceleration = Single.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the Engine_number :");
+                string engine_number = Console.ReadLine();
+
+                Console.WriteLine("Is car started :");
+                bool start = bool.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the time :");
+                int time = Int32.Parse(Console.ReadLine());
+
+
+                CarSpeed s = new CarSpeed();
+                s.setaccelaration(acceleration);
+                s.setengine_number(engine_number);
+                s.settime(time);
+                s.setstart(start);
+                s.showdetails();
+            }
+        }
+    }
+}
---
+++ Task2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace program
+{
+    public class student {
+
+        public string name;
+        public int id;
+        public float algorithm_cgpa;
+        public float oop2_cgpa;
+        public float datastructure_cgpa;
+        public student() { }
+
+        public string Name {
+            get { return name; }
+            set { this.name = value; }
+        }
+        public int Id
+        {
+            get { return id; }
+            set { this.id = value; }
+        }
+        public float Algorithm {
+            get { return algorithm_cgpa; }
+            set { this.algorithm_cgpa = value; }
+        }
+
+        public float Oop2
+        {
+            get { return oop2_cgpa; }
+            set { this.oop2_cgpa = value; }
+        }
+        public float Datastructure
+        {
+            get { return datastructure_cgpa; }
+            set { this.datastructure_cgpa = value; }
+        }
+
+        void showdetails() {
+            Console.WriteLine("Name : " + name);
+            Console.WriteLine("ID   : " + id);
+            Console.WriteLine("CGPA : " + algorithm_cgpa);
+            Console.WriteLine("CGPA : " + oop2_cgpa);
+            Console.WriteLine("CGPA : " + datastructure_cgpa);
+
+        }
+
+        static void Main()
+        {
+
+            Console.WriteLine("Enter the Algorithm CGPA :");
+            float A = Single.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the cgpa :");
+            float cgpa = Single.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the cgpa :");
+            float cgpa = Single.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the name :");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter the id :");
+            int id = Int32.Parse(Console.ReadLine());
+
+            student s = new student();
+            s.Id = id;
+            s.Name = name;
+            s.cgpa = cgpa;
+            s.showdetails();
+
+
+        }
+    }
+    public class GPA : student
+    {
+        static void calculate() {
+                
+        
+        }
+
+
+
+
+
+
+    }
