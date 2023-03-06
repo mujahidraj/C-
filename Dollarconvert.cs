74a704d2c33f79f57bdb25a8b87c00e426c8f229
Mujahid Raj <113965511+mujahidraj@users.noreply.github.com>
2023-03-07 00:37:07 +0600

    Add files via upload

---
+++ Dollarconvert.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        double dollar_amount;
+        int cents;
+        // int compute_cents;
+
+        Console.Write("Enter dollar amount :");
+        dollar_amount = Convert.ToDouble(Console.ReadLine());
+
+        cents = compute_cents(dollar_amount);
+
+        Console.WriteLine("{0} $  = {1} ¢", dollar_amount, cents);
+
+        Console.ReadLine();
+
+    }
+
+    static int compute_cents(double dollar_amount)
+    {
+        return (int)(dollar_amount * 100);
+    }
+}
